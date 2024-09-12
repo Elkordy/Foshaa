091e5f4659d0d5d1899ac6d44ba42ff4ee45b401
Elkordy <muhammadelekrady@gmail.com>
2024-09-12 01:05:17 +0000

    final gcp

---
+++ admin-dashboard/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Welcome to Fosha Admin Dashboard</h1>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+/* Add your styles here */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
